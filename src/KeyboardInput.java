f967e9a60d949682322b2106b2f684db871c080a
arghunter <acgo8888@gmail.com>
2022-04-08 21:59:06 -0700

    Revert "what even is this"
    This reverts commit a5db17a974de468c101b32e6e1e931bb5962d107.

--- src/KeyboardInput.java
+++ src/KeyboardInput.java
@@ -6,14 +6,6 @@ import javax.swing.JFrame;
 
 public class KeyboardInput implements KeyListener {
 	private JFrame frame;
-	private boolean leftPressed = false;
-	private boolean rightPressed = false;
-	private boolean upPressed = false;
-	private boolean downPressed = false;
-	private boolean wPressed = false;
-	private boolean aPressed = false;
-	private boolean sPressed = false;
-	private boolean dPressed = false;
 
 	public KeyboardInput(JFrame frame) 
 	{
@@ -29,116 +21,16 @@ public class KeyboardInput implements KeyListener {
 		
 	}
 
-
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
-			leftPressed=true;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
-			rightPressed=true;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_UP) {
-			upPressed=true;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-			downPressed=true;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_W) {
-			wPressed=true;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_A) {
-			aPressed=true;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_S) {
-			sPressed=true;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_D) {
-			dPressed=true;
-		}
+		// TODO Auto-generated method stub
+		
 	}
-	
+
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
-			leftPressed=false;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
-			rightPressed=false;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_UP) {
-			upPressed=false;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-			downPressed=false;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_W) {
-			wPressed=false;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_A) {
-			aPressed=false;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_S) {
-			sPressed=false;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_D) {
-			dPressed=false;
-		}
+		// TODO Auto-generated method stub
+		
 	}
-
-
-
-
-
-
-
-	public boolean isLeftPressed() {
-		return leftPressed;
-	}
-
-
-
-	public boolean isRightPressed() {
-		return rightPressed;
-	}
-
-
-
-	public boolean isUpPressed() {
-		return upPressed;
-	}
-
-
-
-	public boolean isDownPressed() {
-		return downPressed;
-	}
-
-
-
-	public boolean iswPressed() {
-		return wPressed;
-	}
-
-
-
-	public boolean isaPressed() {
-		return aPressed;
-	}
-
-
-
-	public boolean issPressed() {
-		return sPressed;
-	}
-
-
-
-	public boolean isdPressed() {
-		return dPressed;
-	}
-
-	
-	
 
 }
