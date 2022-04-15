fb2d1b5bf1d183886bf07aeedece4b1ed761335b
Peter Ferolito <89479201+BorealBear@users.noreply.github.com>
2022-04-15 11:14:50 -0700

    still hard

--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -19,5 +19,14 @@ public class MazeGenerator {
 		boolean[][] visited=new boolean[size][size];
 		char[][] maze=new char[2*size+1][2*size+1];
 		
+		for(int i=0;i<2*size+1;i++) {
+			for(int j=0;j<2*size+1;j++) {
+				if(i%2==0 || j%2==0) {
+					maze[i][j]='#';
+				}else {
+					maze[i][j]=' ';
+				}
+			}
+		}
 	}
 }
