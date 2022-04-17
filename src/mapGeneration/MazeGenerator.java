aef26727e9cf1755034533bc4a0b5a265b3170d2
Peter Ferolito <89479201+BorealBear@users.noreply.github.com>
2022-04-17 12:31:35 -0500

    Almost works

--- src/mapGeneration/MazeGenerator.java
+++ src/mapGeneration/MazeGenerator.java
@@ -10,11 +10,12 @@ public class MazeGenerator {
 	}
 	
 	public void generate(int size) {
+		//Right, Down, Left, Up
 		int adjX[]={1,0,-1,0};
 		int adjY[]={0,1,0,-1};
 		
 		LinkedList<Integer[]> stack=new LinkedList<Integer[]>();
-		Integer[] start={0,0};
+		Integer[] start={1,1};
 		stack.add(start);
 		
 		boolean[][] visited=new boolean[size][size];
@@ -29,5 +30,47 @@ public class MazeGenerator {
 				}
 			}
 		}
+		
+		while(stack.size()>0) {
+			Integer[] current=stack.remove(randomNums.nextInt(stack.size()));
+			
+			int numUnvisited=0;
+			for(int i=0;i<4;i++) {
+				if(current[0]+adjY[i]>=0 && current[0]+adjY[i]<size && current[1]+adjX[i]>=0 && current[1]+adjX[i]<size) {
+					if(!visited[current[0]+adjY[i]][current[1]+adjX[i]]) {
+						numUnvisited++;
+					}
+				}
+			}
+			if(numUnvisited>0) {
+				Integer[][] unvisitedNeighbors=new Integer[numUnvisited][2];
+				int unvisitedLeft=numUnvisited;
+				for(int i=0;i<4;i++) {
+					if(current[0]+adjY[i]>=0 && current[0]+adjY[i]<size && current[1]+adjX[i]>=0 && current[1]+adjX[i]<size) {
+						if(!visited[current[0]+adjY[i]][current[1]+adjX[i]]) {
+							unvisitedNeighbors[unvisitedLeft-1][0]=current[0]+adjY[i];
+							unvisitedNeighbors[unvisitedLeft-1][1]=current[1]+adjX[i];
+							unvisitedLeft--;
+						}
+					}
+				}
+				Integer[] newCurrent=unvisitedNeighbors[randomNums.nextInt(numUnvisited)];
+				maze[current[0]+newCurrent[0]][current[1]+newCurrent[1]]=' ';
+				stack.add(current);
+				stack.add(newCurrent);
+				visited[newCurrent[0]][newCurrent[1]]=true;
+			}
+		}
+		for(int i=0;i<2*size+1;i++) {
+			for(int j=0;j<2*size+1;j++) {
+				System.out.print(maze[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String args[]) {
+		MazeGenerator mazeMaker=new MazeGenerator();
+		mazeMaker.generate(20);
 	}
 }
