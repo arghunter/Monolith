55d432eea5730596e8105f79e710422b93fbe4de
Peter Ferolito <89479201+BorealBear@users.noreply.github.com>
2022-04-25 12:48:33 -0700

    I THINK IT WORKS

--- src/GameObjects/mobs/Mob.java
+++ src/GameObjects/mobs/Mob.java
@@ -3,10 +3,11 @@ package GameObjects.mobs;
 import java.awt.Graphics;
 
 import GameObjects.MovingObject;
+import GameObjects.Direction;
 
 public abstract class Mob extends MovingObject {
-	//Health, 
-	private static int[] stats=new int[5];
+	//speed, damage, health, armor, attackspeed, attack range
+	private static int[] stats=new int[6];
 	
 	public Mob(int x,int y,int movementDelay, int[] stats,int id,int width, int height,String name,int numFrames) {
 		super(x,y,movementDelay,id,width,height,name,numFrames);
@@ -17,7 +18,7 @@ public abstract class Mob extends MovingObject {
 	}
 	
 	public void setAllStats(int[] stats) {
-		if(stats.length==5) {
+		if(stats.length==6) {
 			Mob.stats=stats;
 		}
 	}
@@ -25,5 +26,47 @@ public abstract class Mob extends MovingObject {
 		updateAngle(pointX, pointY);
 	}
 
+	public int action(int playerX,int playerY) {
+		int curX=this.getX();
+		int curY=this.getY();
+		int diffX=curX-playerX;
+		int diffY=curY-playerY;
+		int damage=0;
+		if((diffX)*(diffX)+(diffY)*(diffY)<stats[5]*stats[5]) {
+			//ATTACK
+		}else {
+			Direction toMove=Direction.NORTH;
+			if(1.5*Math.abs(diffX)<Math.abs(diffY)) {
+				//Move up down
+				if(diffY>0) {
+					toMove=Direction.NORTH;
+				}else {
+					toMove=Direction.SOUTH;
+				}
+			}else if(1.5*Math.abs(diffY)<Math.abs(diffX)) {
+				//Move left right
+				if(diffX>0) {
+					toMove=Direction.WEST;
+				}else {
+					toMove=Direction.EAST;
+				}
+			}else {
+				//Move diagonal
+				if(diffX>0 && diffY>0) {
+					toMove=Direction.NORTHWEST;
+				}else if(diffX<=0 && diffY>0) {
+					toMove=Direction.NORTHEAST;
+				}else if(diffX>0 && diffY<=0) {
+					toMove=Direction.SOUTHWEST;
+				}else if(diffX<=0 && diffY<=0) {
+					toMove=Direction.SOUTHEAST;
+				}
+			}
+			if(diffX*diffX+diffY*diffY>=64*64) {
+				this.move(toMove);
+			}
+		}
+		return 0;
+	}
 
 }
--- src/GameObjects/mobs/Spider.java
+++ src/GameObjects/mobs/Spider.java
@@ -7,8 +7,8 @@ import java.util.Random;
 import GameObjects.Direction;
 
 public class Spider extends Mob {
-	//speed, damage, health, armor, attackspeed
-	public static int[] stats={20,2,10,0,2000};
+	//speed, damage, health, armor, attackspeed, attack range
+	public static int[] stats={20,2,10,0,2000,40};
 	
 	public Spider(int x,int y,int id,int width,int height) {
 		super(x,y,stats[0],stats,id,width,height,"Spider",3);
