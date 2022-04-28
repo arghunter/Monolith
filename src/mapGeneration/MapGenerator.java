24eead1f9a2ed750e2bb7f68a0997ae6676c2617
Peter Ferolito <89479201+BorealBear@users.noreply.github.com>
2022-04-28 18:57:43 -0500

    Minor edits

--- src/mapGeneration/MapGenerator.java
+++ src/mapGeneration/MapGenerator.java
@@ -9,18 +9,37 @@ public class MapGenerator {
 	public static final int XSIZE=20;
 	public static final int YSIZE=15;
 	
+	//Each room is stored in the form ABCD where ABCD are digits
+	//AB represents the type of room: for each exit, add the corresponding number
+	//CD represents the room number in the corresponding array
+	//
+	// +-- 1 --+
+	// |       |
+	// 8       2
+	// |       |
+	// +-- 4 --+
+	// 
+	
 	ImageSystem image;
 	private char[][] maze;
 	
-	private char[][] roomsStraights;
+	private char[][][][] rooms;
+	
 	
 	public MapGenerator() {
+		generateMaze();
 		generateMap();
+		for(int i=0;i<16;i++) {
+		}
+	}
+	
+	public void generateMaze() {
+		MazeGenerator generator=new MazeGenerator(0);
+		maze=generator.generate(XSIZE,YSIZE);
 	}
 	
 	public void generateMap() {
-		MazeGenerator generator=new MazeGenerator(0);
-		maze=generator.generate(XSIZE,YSIZE);
+		
 	}
 	
 	public void getRoom(int x,int y) {
