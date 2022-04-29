a10f12b4426b610ca8548234ff8caeb9a6d11f4f
Peter Ferolito <89479201+BorealBear@users.noreply.github.com>
2022-04-28 20:45:20 -0500

    paintCompontent and i need to make the rooms bigger

--- src/mapGeneration/MapGeneratorTester.java
+++ src/mapGeneration/MapGeneratorTester.java
@@ -4,22 +4,29 @@ import java.awt.*;
 
 import javax.swing.*;
 
-import general.ImageSystem;
+//import general.ImageSystem;
 
 public class MapGeneratorTester extends JPanel {
-	private ImageSystem image=new ImageSystem(0,0,new ImageIcon("StoneBig.png").getImage());
+
+	//private ImageSystem image=new ImageSystem(0,0,new ImageIcon("StoneBig.png").getImage());
 	
 	public MapGeneratorTester() { 
-
-	}	
-	
-	public static void main(String[] args) {
 		JFrame w = new JFrame("Maze");
 		w.setSize(600,600);
 		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = w.getContentPane();
-		c.add(new MapGeneratorTester());
+		c.add(this);
 		w.setResizable(true);
 		w.setVisible(true);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		g.fillRect(0,0,576,448);
+	}
+	
+	public static void main(String[] args) {
+		MapGeneratorTester gb = new MapGeneratorTester();
 	}
 }
