9278864d0f992f72d65e37719ef52783be73e9a5
Peter Ferolito <89479201+BorealBear@users.noreply.github.com>
2022-04-28 19:50:19 -0500

    Add basic room reading

--- src/mapGeneration/MapGenerator.java
+++ src/mapGeneration/MapGenerator.java
@@ -3,11 +3,17 @@ package mapGeneration;
 import java.awt.*;
 import javax.swing.*;
 import general.ImageSystem;
-import general.SaveSystem;
+import general.FileInput;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class MapGenerator {
 	public static final int XSIZE=20;
 	public static final int YSIZE=15;
+	
+	public static final int ROOMSIZEX=18;
+	public static final int ROOMSIZEY=14;
 	
 	//Each room is stored in the form ABCD where ABCD are digits
 	//AB represents the type of room: for each exit, add the corresponding number
@@ -23,23 +29,49 @@ public class MapGenerator {
 	ImageSystem image;
 	private char[][] maze;
 	
-	private char[][][][] rooms;
+	private int[][][][] rooms;
 	
+	private int[] numRooms = new int[16];
 	
 	public MapGenerator() {
 		generateMaze();
-		generateMap();
-		for(int i=0;i<16;i++) {
+		try {
+			readRooms();
+		} catch(Exception e) {
+			System.out.println("Can't read rooms from files");
+			e.printStackTrace();
 		}
-		
+		generateMap();
 	}
 	
-	public void generateMaze() {
+	private void readRooms() throws FileNotFoundException {
+		int maxRooms=-1;
+		for(int i=0;i<16;i++) {
+			FileInput input = new FileInput("Rooms"+(i<10?"0":"")+i+".txt");
+			int numRooms=Integer.parseInt(input.next());
+			maxRooms=(numRooms>maxRooms?numRooms:maxRooms);
+			
+		}
+		rooms=new int[16][maxRooms][ROOMSIZEY][ROOMSIZEX];
+		for(int i=0;i<16;i++) {
+			FileInput input = new FileInput("Rooms"+(i<10?"0":"")+i+".txt");
+			for(int j=0;j<numRooms[i];j++) {
+				for(int k=0;k<ROOMSIZEY;k++) {
+					String s=input.next();
+					for(int m=0;m<ROOMSIZEX;m++) {
+						rooms[i][j][k][m]=Integer.parseInt(s.substring(m*2,m*2+2));
+					}
+				}
+			}
+		}
+	}
+	
+	private void generateMaze() {
 		MazeGenerator generator=new MazeGenerator(0);
 		maze=generator.generate(XSIZE,YSIZE);
 	}
 	
-	public void generateMap() {
+	private void generateMap() {
 		
 	}
 	
