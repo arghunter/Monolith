3064029d589ceb72b763fad8bc0af110fa73a05c
arghunter <acgo8888@gmail.com>
2022-05-05 23:48:20 -0700

    atupidity

--- src/ui/ArsenalMenu.java
+++ src/ui/ArsenalMenu.java
@@ -2,10 +2,14 @@ package ui;
 
 import java.awt.Point;
 import java.awt.Polygon;
+import java.util.ArrayList;
+
+import GameObjects.Player.Inventory;
 
 public class ArsenalMenu {
 	Polygon shape;
-	public ArsenalMenu() 
+	RenderableMenuItem[] arsenalItems;
+	public ArsenalMenu(Inventory inventory) 
 	{
 		Point[] shapeCoords={new Point(1476,727),new Point(1446,799),new Point(1391,854),new Point(1319,884),new Point(1241,884),new Point(1169,854),new Point(1114,799),new Point(1084,727),new Point(1084,649),new Point(1114,577),new Point(1169,522),new Point(1241,492),new Point(1319,492),new Point(1391,522),new Point(1446,577),new Point(1476,649)};
 		shape=new Polygon();
@@ -13,6 +17,17 @@ public class ArsenalMenu {
 		{
 			shape.addPoint(shapeCoords[i].x, shapeCoords[i].y);
 		}
+		arsenalItems=new RenderableMenuItem[inventory.getArsenal().length];
+	
+		for(int i=0;i<arsenalItems.length;i++) 
+		{
+			if(inventory.getArsenal()[i]!=null) 
+			{
+				
+			}
+		}
+		
+		
 	}
 
 }
