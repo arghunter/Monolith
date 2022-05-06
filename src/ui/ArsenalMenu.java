fac6817fe290cedfa3dfb5141f5cf28a06b911c2
arghunter <acgo8888@gmail.com>
2022-05-05 23:42:10 -0700

    fancy shape

--- src/ui/ArsenalMenu.java
+++ src/ui/ArsenalMenu.java
@@ -1,5 +1,18 @@
 package ui;
 
+import java.awt.Point;
+import java.awt.Polygon;
+
 public class ArsenalMenu {
+	Polygon shape;
+	public ArsenalMenu() 
+	{
+		Point[] shapeCoords={new Point(1476,727),new Point(1446,799),new Point(1391,854),new Point(1319,884),new Point(1241,884),new Point(1169,854),new Point(1114,799),new Point(1084,727),new Point(1084,649),new Point(1114,577),new Point(1169,522),new Point(1241,492),new Point(1319,492),new Point(1391,522),new Point(1446,577),new Point(1476,649)};
+		shape=new Polygon();
+		for(int i=0;i<shapeCoords.length;i++) 
+		{
+			shape.addPoint(shapeCoords[i].x, shapeCoords[i].y);
+		}
+	}
 
 }
