c6894424db6c776565d911c83c5e786c8259484b
arghunter <acgo8888@gmail.com>
2022-05-10 21:44:20 -0700

    minor

--- src/ui/RenderableMenuItem.java
+++ src/ui/RenderableMenuItem.java
@@ -40,9 +40,19 @@ public class RenderableMenuItem implements ActionListener {
 	private Button[] itemButtons=new Button[0];
 	private boolean isSelected=false;
 	private boolean isHovering=false;
-	
+	private boolean showDescription=true;
+	public boolean isShowDescription() {
+		return showDescription;
+	}
+	public void setShowDescription(boolean showDescription) {
+		this.showDescription = showDescription;
+	}
 	public RenderableMenuItem(Item item,int x, int y,JPanel panel) 
 	{
+		if(item==null) 
+		{
+			throw new IllegalArgumentException("Null Item");
+		}
 		this.item=item;
 		this.x=x;
 		this.y=y;
@@ -99,7 +109,30 @@ public class RenderableMenuItem implements ActionListener {
 		}
 		g.drawImage(iconImg.getImage(), 0, 0, null);
 		image=new ImageSystem(x+5,y+5,img);
+		image.setScale(256.0/image.getWidth(), 256.0/image.getHeight());
+		image.move(image.getWidth()/2, image.getHeight()/2);
+		int tx=x;
+		int ty=y;
 
+		Point[] points= {new Point(tx,ty),new Point(tx+image.getWidth()+10,ty),new Point(tx+image.getWidth()+10,ty+image.getHeight()+10),new Point(tx,ty+image.getHeight()+10)};
+		button=new Button(points,new Color(0f,0f,0f,0f));
+		panel.add(button);
+		button.addActionListener(this);
+		button.setHoverEffectsOn(false);
+		
+	}
+	public RenderableMenuItem(int x, int y,JPanel panel) 
+	{
+		this.item=null;
+		this.x=x;
+		this.y=y;
+		
+		actionListeners=new ActionListener[0];
+		
+		BufferedImage img=new BufferedImage(256,256,BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g=img.createGraphics();
+		image=new ImageSystem(x+5,y+5,img);
+		image.setScale(256.0/image.getWidth(), 256.0/image.getHeight());
 		image.move(image.getWidth()/2, image.getHeight()/2);
 		int tx=x;
 		int ty=y;
@@ -116,7 +149,8 @@ public class RenderableMenuItem implements ActionListener {
 	{
 		button.draw(g, JPanelX, JPanelY);
 		this.isHovering=button.isHovering();
-		image.drawImage(g);
+		if(item!=null)
+			image.drawImage(g);
 		Font text=null;
 		try {
 			text = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Exo_2/static/Exo2-Medium.ttf"));
@@ -128,35 +162,39 @@ public class RenderableMenuItem implements ActionListener {
 		}
 		g.setFont(text.deriveFont(30f));
 		g.setColor(Constants.textColor);
-		if(item.getType()==ItemType.CONSUMABLE) 
+		if(item!=null) 
 		{
+			if(item.getType()==ItemType.CONSUMABLE) 
+			{
 
-			Consumable consumable=(Consumable) item;
-			String count="x"+consumable.getCount();
-			FontMetrics metrics=g.getFontMetrics();
-			g.drawString(count, image.getWidth()+x-metrics.stringWidth(count), y+metrics.getHeight());
-		}else if(item.getType()==ItemType.MATERIAL) 
-		{
+				Consumable consumable=(Consumable) item;
+				String count="x"+consumable.getCount();
+				FontMetrics metrics=g.getFontMetrics();
+				g.drawString(count, image.getWidth()+x-metrics.stringWidth(count), y+metrics.getHeight());
+			}else if(item.getType()==ItemType.MATERIAL) 
+			{
 
-			Material material=(Material) item;
-			String count="x"+material.getCount();
-			FontMetrics metrics=g.getFontMetrics();
-			g.drawString(count.substring(0,count.indexOf('.')), image.getWidth()+x-metrics.stringWidth(count.substring(0,count.indexOf('.'))), y+metrics.getHeight());
-		}else if(item.getType()==ItemType.BLUEPRINT) 
-		{
+				Material material=(Material) item;
+				String count="x"+material.getCount();
+				FontMetrics metrics=g.getFontMetrics();
+				g.drawString(count.substring(0,count.indexOf('.')), image.getWidth()+x-metrics.stringWidth(count.substring(0,count.indexOf('.'))), y+metrics.getHeight());
+			}else if(item.getType()==ItemType.BLUEPRINT) 
+			{
 
-			Blueprint material=(Blueprint) item;
-			String count="x"+material.getCount();
-			FontMetrics metrics=g.getFontMetrics();
-			g.drawString(count.substring(0,count.indexOf('.')), image.getWidth()+x-metrics.stringWidth(count.substring(0,count.indexOf('.'))), y+metrics.getHeight());
-		}else 
-		{
+				Blueprint material=(Blueprint) item;
+				String count="x"+material.getCount();
+				FontMetrics metrics=g.getFontMetrics();
+				g.drawString(count.substring(0,count.indexOf('.')), image.getWidth()+x-metrics.stringWidth(count.substring(0,count.indexOf('.'))), y+metrics.getHeight());
+			}else 
+			{
 
-			String count="x1";
-			FontMetrics metrics=g.getFontMetrics();
-			g.drawString(count, image.getWidth()+x-metrics.stringWidth(count), y+metrics.getHeight());
+				String count="x1";
+				FontMetrics metrics=g.getFontMetrics();
+				g.drawString(count, image.getWidth()+x-metrics.stringWidth(count), y+metrics.getHeight());
+			}
 		}
-		if(button.isHovering()||this.isSelected) 
+
+		if((button.isHovering()||this.isSelected)&&(item!=null&&showDescription)) 
 		{
 			g.setColor(new Color(0.4f,0.4f,0.4f,0.5f));
 			g.fillRect(x+2,y-1+11*image.getHeight()/12,image.getWidth()+6,image.getHeight()/12+10);
@@ -219,6 +257,24 @@ public class RenderableMenuItem implements ActionListener {
 		
 	}
 
+	public ImageSystem getImage() {
+		return image;
+	}
+	public Button getButton() {
+		return button;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public ActionListener[] getActionListeners() {
+		return actionListeners;
+	}
+	public Button[] getItemButtons() {
+		return itemButtons;
+	}
 	public void translate(int x,int y) 
 	{
 		button.translate(x, y);
