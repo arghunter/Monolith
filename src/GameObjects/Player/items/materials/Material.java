9c40117bb8c8bf792eea94d75248648204d19d56
arghunter <acgo8888@gmail.com>
2022-05-13 20:50:43 -0700

    final comments

--- src/GameObjects/Player/items/materials/Material.java
+++ src/GameObjects/Player/items/materials/Material.java
@@ -1,23 +1,28 @@
+//Author: Adithya Giri
+//Date: 5/11/22
+//Rev: 01
+//Notes: A material in Monolisth
 package GameObjects.Player.items.materials;
 
 import GameObjects.Player.items.Item;
 import GameObjects.Player.items.ItemType;
 
 public class Material extends Item {
-
+	//Fields
 	double count;
+	//Constructor
 	public Material(String name,int tier,double count) {
 		super(name, ItemType.MATERIAL,tier);
 		this.count=Math.ceil(count);
 		
 	}
-	
+	//Adds a material to this one
 	public void add(Material material) 
 	{
 		count+=Math.round(material.getCount());
 		
 	}
-
+	//Consumes this material
 	public Material consume(Material material) 
 	{
 		if(!this.equals(material)) 
@@ -32,14 +37,15 @@ public class Material extends Item {
 		}
 		return null;
 	}
-
+	//Returns Count
 	public double getCount() {
 		return count;
 	}
-
+	//Sets the count
 	public void setCount(double count) {
 		this.count = count;
 	}
+	//to string for save data parsing
 	public String toString() 
 	{
 		return "(Item:"+super.getName()+"/"+super.getTier()+"/"+super.getType()+"/"+count;
