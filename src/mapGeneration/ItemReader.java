3047ecfee084b04006ca398328e4327250eb8172
Peter Ferolito <89479201+BorealBear@users.noreply.github.com>
2022-05-19 12:47:46 -0700

    random changes

--- src/mapGeneration/ItemReader.java
+++ src/mapGeneration/ItemReader.java
@@ -18,9 +18,15 @@ import GameObjects.Player.items.materials.*;
 import GameObjects.Player.items.weapons.*;
 
 public class ItemReader {
-	public MeleeWeapon[] meleeWeapons;
+	public MeleeWeapon[][] meleeWeapons;
+	public Consumable[][] consumables;
+	public Material[][] materials;
+	public LongRangeWeapon[][] longRangeWeapons;
 	
 	public ItemReader() {
+		for(int i=0;i<11;i++) {
+			
+		}
 		try{
 			readItems();
 		}catch (Exception e) {
@@ -29,6 +35,34 @@ public class ItemReader {
 	}
 	
 	private void readItems() throws FileNotFoundException{
+		FileInput input=new FileInput("ItemsAndMobs/Consumables.txt");
+		consumables=new Consumable[10][];
+		for(int i=0;i<10;i++) {
+			int tempNum=Integer.parseInt(input.next());
+			consumables[i]=new Consumable[tempNum];
+		}
 		
+		input=new FileInput("ItemsAndMobs/Materials.txt");
+		for(int i=0;i<10;i++) {
+			
+		}
+		
+		input=new FileInput("ItemsAndMobs/MeleeWeapons.txt");
+		
+		meleeWeapons=new MeleeWeapon[10][];
+		for(int i=0;i<10;i++) {
+			int tempNum=Integer.parseInt(input.next());
+			meleeWeapons[i]=new MeleeWeapon[tempNum];
+			for(int j=0;j<tempNum;j++) {
+				String item=input.next();
+				item.replace("_"," ");
+				int[] positions=new int[5];
+				positions[0]=item.indexOf('~');
+				for(int k=1;k<5;k++) {
+					positions[k]=item.indexOf('~', positions[k-1]+1);
+				}
+				//meleeWeapons[i][j]=new MeleeWeapon();
+			}
+		}
 	}
 }
