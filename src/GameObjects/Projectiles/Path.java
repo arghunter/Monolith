fe230e5dd9cf9700a2fd89c44d51fc2d2f33d69c
AdithyaG <incredibleatha2@gmail.com>
2022-05-21 23:06:40 -0700

    Update Path.java

--- src/GameObjects/Projectiles/Path.java
+++ src/GameObjects/Projectiles/Path.java
@@ -22,6 +22,14 @@ public class Path {
 	}
 	//Implement later
 	public void rotate(Point p, double angle) {
-		
+		ArrayList<Point> newArr = new ArrayList<Point>()
+		for(int i = 0; i < arr.size(); i++) {
+			int height = arr.get(i).x-p.y;
+			int width = arr.get(i).x-p.x;
+			int x =(int) ((height*Math.cos(angle) - width*Math.sin(angle)) + p.x);
+			int y =(int) ((height*Math.sin(angle) + width*Math.cos(angle)) + p.y);
+			newArr.add(new Point(x,y));
+		}
+		this.arr = newArr;
 	}
 }
